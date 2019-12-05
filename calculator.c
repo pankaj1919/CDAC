889d617e34e3b613b3f8fb446f71cf652232d719
pankajsakhare19 <pankajsakhare91@gmail.com>
2019-12-05 06:54:10 -0800

    done

--- calculator.c
+++ calculator.c
@@ -6,7 +6,6 @@ printf("Simple Calculator\n");
 float a=10, b=2;
 float res;
 printf("Enter any two number\n");
-scanf("%f %f",&a,&b);
 printf("Entered Number is: %d and %d\n",a,b);
 res=a+b;
 printf("sum of two no is: %f\n", res);
